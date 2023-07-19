d64fd657e4506f2e719e8e0a4972bccf144dd6f3
BAGE BHARATH KALYAN <106260720+bharath630@users.noreply.github.com>
2023-07-19 05:19:00 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-
-    const slider = document.querySelector('.slider');
-    const images = document.querySelectorAll('.slider img');
-    const prevArrow = document.querySelector('.pre');
-    const nextArrow = document.querySelector('.next');
-    const displayImg = document.querySelector('.display-img');
-
-    images.forEach(img => {
-      img.addEventListener('click', () => {
-        displayImg.src = img.src;
-        displayImg.style.display = "block";
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".slider img");
+const body = document.body;
+const displayImg = document.querySelector(".display-img");
+const imageInput = document.getElementById("imageInput");
+const imageGallery = document.getElementById("imageGallery");
+add();
+function add() {
+  imageInput.addEventListener("change", function () {
+    imageGallery.innerHTML = "";
+    for (const file of imageInput.files) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const imgElement = document.createElement("img");
+        imgElement.src = e.target.result;
+        imgElement.alt = "Uploaded Image";
+        const firstImage = slider.querySelector("img");
+        slider.insertBefore(imgElement, firstImage);
+        imgElement.addEventListener("click", function () {
+          images.forEach((img) => {
+            img.style.display = "none";
+          });
+          displayImg.src = this.src;
+          displayImg.style.display = "block";
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+}
+display();
+function display() {
+  images.forEach((img) => {
+    img.addEventListener("click", function () {
+      images.forEach((img) => {
+        img.style.display = "none";
       });
+      displayImg.src = this.src;
+      displayImg.style.display = "block";
     });
-    displayImg.addEventListener('click', () => {
-      displayImg.style.display = "none";
+  });
+}
+restore();
+function restore() {
+  displayImg.addEventListener("click", function () {
+    displayImg.style.display = "none";
+    images.forEach((img) => {
+      img.style.display = "block";
     });
-    prevArrow.addEventListener('click', () => {
-      slider.scrollLeft -= slider.offsetWidth;
-    });
-    nextArrow.addEventListener('click', () => {
-      slider.scrollLeft += slider.offsetWidth;
-    });
-
-//...........................
-    
+    display();
+    body.classList.remove("hide-scrollbar");
+  });
+}
